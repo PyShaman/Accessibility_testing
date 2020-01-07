9c89acd8280f6afafc68527d6dc4d56c691ef72a
PyShaman <michal.bek@gmail.com>
2020-01-07 12:10:27 +0100

    Accessibility testing

--- test_01_wgac.py
+++ test_01_wgac.py
@@ -23,40 +23,40 @@ def test_emakina_group_home_page():
     AS = AccessSite()
     driver.get(SITES[0])
     site_name = "Home Page"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[0]}", f"emakina_group_{site_name}.txt")
 
 
 def test_emakina_group_agencies_site():
     AS = AccessSite()
     driver.get(SITES[1])
     site_name = "Agencies"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[1]}", f"emakina_group_{site_name}.txt")
 
 
 def test_emakina_group_services_site():
     AS = AccessSite()
     driver.get(SITES[2])
     site_name = "Services"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[2]}", f"emakina_group_{site_name}.txt")
 
 
 def test_emakina_group_offices_site():
     AS = AccessSite()
     driver.get(SITES[3])
     site_name = "Offices"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[3]}", f"emakina_group_{site_name}.txt")
 
 
 def test_emakina_group_press_site():
     AS = AccessSite()
     driver.get(SITES[4])
     site_name = "Press"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[4]}", f"emakina_group_{site_name}.txt")
 
 
 def test_emakina_group_media_relations_site():
     AS = AccessSite()
     driver.get(SITES[5])
     site_name = "Media_relations"
-    AS.inject_axe(driver, site_name, f"emakina_group_{site_name}.txt")
+    AS.inject_axe(driver, f"{site_name}:{SITES[5]}", f"emakina_group_{site_name}.txt")
     driver.quit()
